9a6e94fc8e843208c4fc16972db9069b7b744a7a
CarlinCharge <sprintman210@gmail.com>
2019-03-25 11:06:44 -0400

    refine sign in and out redux

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,14 +1,34 @@
 export const signIn = () => {
-  return {
-    type: 'SIGN_IN',
-    email: 'bill@example.com',
-    displayName: 'Bill Murray',
-    photoURL: 'http://www.fillmurray.com/200/200',
-    uid: 'firstUser'
+  return (dispatch) => {
+    dispatch({type: 'ATTEMPTING_LOGIN'});
+    setTimeout(()=> {
+      dispatch(signedIn({
+        email: 'bill@example.com', 
+        displayName: 'Bill Murray', 
+        photoURL: 'http://www.fillmurray.com/200/200', 
+        uid: 'firstUser'}));
+    }, 2000);
   };
 };
 
 export const signOut = () => {
+  return (dispatch) => {
+    dispatch({type: 'ATTEMPTING_SIGNOUT' });
+    dispatch(signedOut());
+  };
+};
+
+const signedIn = (user) => {
+  return {
+    type: 'SIGN_IN',
+    email: user.email,
+    displayName: user.displayName,
+    photoURL: 'http://www.fillmurray.com/200/200',
+    uid: user.uid
+  };
+};
+
+const signedOut = () => {
   return {
     type: 'SIGN_OUT'
   };
